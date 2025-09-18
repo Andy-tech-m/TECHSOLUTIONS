132123a8d99ef987a1b568be87f8d519d7e5f651
Andy-tech-max <conculito@gmail.com>
2025-09-18 16:34:44 -0500

    login

---
+++ Frontend/frontend/src/pages/LoginPages.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './LoginPage.css'
+
+export default class LoginPage extends Component {
+    render() {
+        return (
+            <div>LoginPage</div>
+        )
+    }
+}
